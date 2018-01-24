68934d757ae7849a698043c2b45f03ebee069afd
neechie <neechieg@gmail.com>
2018-01-24 10:49:22 -0500

    hello

---
+++ emergency_compliment/src/hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Hello extends Component {
+    render() {
+      return (
+       
+          <p>I am Neechie  </p>
+        </div>
+      );
+    }
+  }
+  
